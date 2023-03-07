3012f46c7e635ec97a713a756ef0dbe20de1babd
LucianoOga <70287320+LucianoOga@users.noreply.github.com>
2023-03-07 16:04:01 -0300

    Add files via upload

---
+++ Javascript/Exercise2.js
@@ -0,0 +1,17 @@
+
+//1
+const cadenas = ["Apple", "Orange", "Banana", "Strawberry", "Grape", "Mango", "Melon",    	  	                       "Pineapple", "Peach","Peach"];
+
+const cadenasP = cadenas.filter(cadena => cadena.includes('p') || cadena.includes('P'));
+
+console.log(cadenasP);
+
+//2
+const indexGrape = datos.indexOf("Grape");
+	
+if(indexGrape !== -1){
+	console.log("Grape");
+}else{
+	console.log("The Grape string is not in the array.");
+}
+
